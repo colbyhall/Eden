54743c771f9ef611eb450bccc4a8f946eae29e72
Colby Hall <me@cobeh.com>
2019-05-11 18:03:11 -0700

    cleaned up editor.cpp

--- src/editor.cpp
+++ src/editor.cpp
@@ -17,22 +17,30 @@
 
 static void editor_key_pressed(void* owner, Event* event) {
 	Editor_State* editor = (Editor_State*)owner;
+	Input_State* input = &editor->input_state;
 
 	Buffer* buffer = &editor->loaded_buffers[0];
 	if (!buffer) {
 		return;
 	}
-    bool edited = false;
 	switch (event->key_code) {
 	case KEY_ENTER:
 		buffer_add_char(buffer, '\n');
-        edited = true;
 		break;
 	case KEY_LEFT:
-		buffer_move_cursor_horizontal(buffer, -1);
+		if (input->ctrl_is_down) {
+			buffer_seek_horizontal(buffer, false);
+		} else {
+			buffer_move_cursor_horizontal(buffer, -1);
+		}
 		break;
 	case KEY_RIGHT:
-		buffer_move_cursor_horizontal(buffer, 1);
+		if (input->ctrl_is_down) {
+			buffer_seek_horizontal(buffer, true);
+		}
+		else {
+			buffer_move_cursor_horizontal(buffer, 1);
+		}
 		break;
 	case KEY_UP:
 		buffer_move_cursor_vertical(buffer, -1);
@@ -48,17 +56,11 @@ static void editor_key_pressed(void* owner, Event* event) {
 		break;
 	case KEY_BACKSPACE:
 		buffer_remove_before_cursor(buffer);
-        edited = true;
 		break;
 	case KEY_DELETE:
 		buffer_remove_at_cursor(buffer);
-        edited = true;
 		break;
 	}
-	// editor_bring_view_to_cursor(current_view, buffer);
-	if (edited) {
-        parse_syntax(buffer);
-    }
 }
 
 static void editor_char_entered(void* owner, Event* event) {
@@ -146,66 +148,13 @@ void editor_draw(Editor_State* editor) {
 		draw_rect(x0, y0, x1, y1, 0x052329);
 	}
 
-	// @NOTE(Colby): Command Bar and buffer drawing
 	{
-		const float font_height = FONT_SIZE;
-		const Vector2 padding = v2(font_height / 2.f);
-		const float bar_height = font_height + padding.y;
-		{
-			const float x0 = 0.f;
-			const float y0 = 0.f;
-			const float x1 = x0 + window_width;
-			const float y1 = y0 + window_height - bar_height;
-			draw_buffer_view(&editor->loaded_buffers[0], x0, y0, x1, y1, editor->loaded_font);
-		}
-		
-		{
-			immediate_begin();
-			const float x0 = 0.f;
-			const float y0 = window_height - bar_height;
-			const float x1 = x0 + window_width;
-			const float y1 = y0 + bar_height;
-			immediate_quad(x0, y0, x1, y1, 0x052329);
-
-			const float x = x0 + padding.x / 2.f;
-			const float y = y0 + padding.y / 2.f;
-			immediate_string("esc", x, y, 0xd6b58d, editor->loaded_font);
-			immediate_flush();
-		}
+		const float x0 = 0.f;
+		const float y0 = 0.f;
+		const float x1 = x0 + window_width;
+		const float y1 = y0 + window_height;
+		draw_buffer_view(&editor->loaded_buffers[0], x0, y0, x1, y1, editor->loaded_font);
 	}
-
-#if BUILD_DEBUG
-	{
-		char buffer[256];
-		sprintf_s(
-			buffer, 256, "Draw Calls: %i\nVerts Drawn: %llu\nVerts Culled: %llu\nDT: %.6f\nAllocations: %llu\nAllocated: %f KB",
-			draw_calls, verts_drawn, verts_culled, editor->dt, memory_num_allocations(), memory_amount_allocated() / 1024.f
-		);
-		Vector2 string_size = get_draw_string_size(buffer, editor->loaded_font);
-		Vector2 padding = v2(10.f);
-		float x = os_window_width() - (string_size.x + padding.x);
-		float y = 0.f;
-
-		immediate_begin();
-		{
-
-			float x0 = x;
-			float y0 = y;
-			float x1 = x0 + string_size.x + padding.x;
-			float y1 = y0 + string_size.y + padding.y;
-
-			immediate_quad(x0, y0, x1, y1, 0xAAAAAA);
-		}
-
-		{
-			float x0 = x + padding.x / 2.f;
-			float y0 = y + padding.y / 2.f;
-			immediate_string(buffer, x0, y0, 0xFFFFFF, editor->loaded_font);
-		}
-		immediate_flush();
-	}
-#endif
-
 	render_frame_end();
 }
 
