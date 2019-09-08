8a46965836545ed4d2017d0b06dc7a13134ad284
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2019-09-08 01:56:14 -0400

    Put lexemes in buffer for rendering

--- src/buffer.h
+++ src/buffer.h
@@ -4,6 +4,7 @@
 #include <ch_stl/gap_buffer.h>
 #include <ch_stl/hash.h>
 #include "draw.h"
+#include "parsing.h"
 
 using Buffer_ID = usize;
 
@@ -16,6 +17,9 @@ struct Buffer {
 	ch::Gap_Buffer<u32> gap_buffer;
 	ch::Path full_path;
 	ch::Array<usize> eol_table;
+
+    Lexeme* lexemes = nullptr;
+    f64 parse_time;
 
 	Buffer();
 	Buffer(Buffer_ID _id);
